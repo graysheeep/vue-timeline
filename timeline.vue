8d58da19407fe108b12bac21f8e7692c84348855
graysheeep <graysheeep@gmail.com>
2016-04-19 16:37:27 +0800

    main timeline upload

---
+++ timeline.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="vux-timeline">
+		<ul>
+			<slot></slot>
+		</ul>
+	</div>
+</template>
+
+<style lang="less">
+	.vux-timeline {
+		background-color:#EEE;
+		padding:1rem;
+
+		ul li {
+			list-style: none;
+		}
+
+	}
+</style>
+
+<script>
+	import TimelineItem from './timeline-item'
+
+	export default {
+	  components: {
+	    TimelineItem
+	  },
+	  ready () {
+        this.setChildProps()
+      },
+	  methods: {
+	    setChildProps () {
+	      const len = this.$children.length
+	      this.$children.forEach((child, index) => {
+	        child.isLast = index === len - 1
+	        child.isFirst = index === 0
+	      })
+	    }
+	  }
+	}
+</script>
